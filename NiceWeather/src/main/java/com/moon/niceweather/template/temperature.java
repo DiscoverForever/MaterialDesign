fbb0ae83163a86b22a3aa851234e44e624dc16f2
tangyueyang <595740536@qq.com>
2016-12-28 09:53:06 +0800

    添加modle类

---
+++ NiceWeather/src/main/java/com/moon/niceweather/template/temperature.java
@@ -0,0 +1,25 @@
+package com.moon.niceweather.template;
+
+/**
+ * Created by adminr on 2016/11/16.
+ */
+public class Temperature {
+    private String jg;
+    private String jb;
+
+    public String getJg() {
+        return jg;
+    }
+
+    public void setJg(String jg) {
+        this.jg = jg;
+    }
+
+    public String getJb() {
+        return jb;
+    }
+
+    public void setJb(String jb) {
+        this.jb = jb;
+    }
+}
