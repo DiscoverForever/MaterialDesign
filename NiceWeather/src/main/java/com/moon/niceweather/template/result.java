62ca07a0b4d620ad3249557f7b8d3dc822e62450
tangyueyang <595740536@qq.com>
2016-12-28 09:54:51 +0800

    result commit

---
+++ NiceWeather/src/main/java/com/moon/niceweather/template/result.java
@@ -0,0 +1,16 @@
+package com.moon.niceweather.template;
+
+/**
+ * Created by adminr on 2016/11/16.
+ */
+public class Result {
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+}
