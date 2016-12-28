7b9460083d88b66d1bb1be9278cf66f06a07ec27
tangyueyang <595740536@qq.com>
2016-12-28 15:44:58 +0800

    增加近一周 近一天 天气

--- NiceWeather/src/main/java/com/moon/niceweather/MainActivity.java
+++ NiceWeather/src/main/java/com/moon/niceweather/MainActivity.java
@@ -1,48 +1,99 @@
 package com.moon.niceweather;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.NetworkImageView;
-import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
-import com.moon.niceweather.activity.FullscreenActivity;
 import com.moon.niceweather.activity.LoginActivity;
+import com.moon.niceweather.adapter.WeatherListAdapter;
+import com.moon.niceweather.template.MyLocation;
+import com.moon.niceweather.template.ResponseTemplate;
+import com.moon.niceweather.template.Temperature;
 import com.moon.niceweather.utils.VolleyUtil;
 import com.yalantis.phoenix.PullToRefreshView;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    @ViewInject(R.id.hs_day_details)
+    private LinearLayout hs_day_details;
     @ViewInject(R.id.pull_to_refresh)
     private PullToRefreshView mPullToRefreshView;
     @ViewInject(R.id.imageView)
     private NetworkImageView netImageView;
+    @ViewInject(R.id.lv_weather)
+    private ListView lv_weather;
+    @ViewInject(R.id.tv_city_name)
+    private TextView tv_city_name;
+    @ViewInject(R.id.tv_temperature)
+    private TextView tv_temperature;
+    @ViewInject(R.id.tv_weather)
+    private TextView tv_weather;
+
+
+    /* GPS Constant Permission */
+    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 12;
+
+    /* Position */
+    private static final int MINIMUM_TIME = 1000;  // 1s 定位时间触发间隔(ms)
+    private static final int MINIMUM_DISTANCE = 1; // 1m 定位位置触发精度(m)
+
+    private Context context;
+    private LocationManager locationManager;
+    private String locationProvider;
+    private Location location;
+    /* 当前城市名 */
+    private String cityName = "";
+    /* 当前温度 */
+    private String temperature_now = "";
+    /* 当前天气状况 */
+    private String weather = "";
+    private LayoutInflater mLayoutInflater;
+    private View galleryItem = null;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -67,13 +118,204 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        initPullToRefreshView();
-//        sendEmial();
-//        getData();
+        init();
+
+    }
+
+    /**
+     * 初始化
+     */
+    private void init() {
+        context = this;
+        initLocation();//初始化定位
+        initPullToRefreshView();//初始化下拉刷新
+    }
+
+    /**
+     * 初始化当天温度详细
+     * @param response
+     */
+    private void initHorizontalScrollView(ResponseTemplate response) {
+        mLayoutInflater = LayoutInflater.from(context);
+
+        ArrayList<Temperature> temperatureList = response.getResult().getData().getF3h().getTemperature();
+        for (int i = 0; i < temperatureList.size(); i++) {
+            galleryItem = mLayoutInflater.inflate(R.layout.gallery_item, null);
+            TextView hour = (TextView) galleryItem.findViewById(R.id.tv_hour);
+            ImageView weatherImg = (ImageView) galleryItem.findViewById(R.id.iv_weather);
+            TextView temperature = (TextView) galleryItem.findViewById(R.id.tv_temperature);
+            hour.setText(getTime(temperatureList.get(i).getJg()));
+            temperature.setText(temperatureList.get(i).getJb());
+            hs_day_details.addView(galleryItem);
+        }
+
+    }
+
+    /**
+     * 获取时间上下午
+     * @param time
+     * @return
+     */
+    public String getTime(String time) {
+
+        if (Integer.parseInt(time.substring(8, 10)) < 12) {
+
+            time = "上午" + time.substring(8, 10);
+        } else {
+            time = "下午" + time.substring(8, 10);
+        }
+
+        return time;
+    }
+
+    /**
+     * 初始化近一周天气
+     */
+    private void initNearlyWeekWeather(ResponseTemplate data) {
+        WeatherListAdapter adapter = new WeatherListAdapter(this, data);
+        lv_weather.setAdapter(adapter);
+    }
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation() {
+        locationManager = (LocationManager) this.getSystemService(context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        locationProvider = locationManager.getBestProvider(criteria, true);//选择最优定位
+        // API 23: we have to check if ACCESS_FINE_LOCATION and/or ACCESS_COARSE_LOCATION permission are granted
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // No one provider activated: prompt GPS
+            if (locationProvider == null || locationProvider.equals("")) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+
+            // At least one provider activated. Get the coordinates
+            locationManager.requestLocationUpdates(locationProvider, MINIMUM_TIME, MINIMUM_DISTANCE, locationListener);
+            switch (locationProvider) {
+                case "passive":
+                    location = locationManager.getLastKnownLocation(locationProvider);
+                    if (location != null) {
+
+                    } else {
+                        Toast.makeText(MainActivity.this, "请打开网络开关", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+                case "network":
+                    location = locationManager.getLastKnownLocation(locationProvider);
+                    if (location != null) {
+                        VolleyUtil volleyUtil = new VolleyUtil(this, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject jsonObject) {
+                                Gson gson = new Gson();
+                                MyLocation myLocation = gson.fromJson(jsonObject.toString(), MyLocation.class);
+                                cityName = myLocation.getResult().getAddressComponent().getCity();
+                                Message msg = new Message();
+                                msg.what = 1;
+                                mhandler.sendMessage(msg);
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        volleyUtil.doGet(volleyUtil.getLocationUrl("" + location.getLatitude(), "" + location.getLongitude()));
+                    } else {
+                        Toast.makeText(MainActivity.this, "请打开网络开关", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+
+                case "gps":
+                    location = locationManager.getLastKnownLocation(locationProvider);
+                    Toast.makeText(context, "" + location, Toast.LENGTH_SHORT).show();
+                    if (location != null) {
+                        String locationStr = "维度：" + location.getLatitude() + "\n"
+                                + "经度：" + location.getLongitude();
+                        Toast.makeText(MainActivity.this, locationStr, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "请打开GPS开关", Toast.LENGTH_SHORT).show();
+                    }
+                    break;
+                default:
+
+                    break;
+
+            }
+
+            // One or both permissions are denied.
+        } else {
+            Toast.makeText(MainActivity.this, "拒绝定位", Toast.LENGTH_SHORT).show();
+
+            // The ACCESS_COARSE_LOCATION is denied, then I request it and manage the result in
+            // onRequestPermissionsResult() using the constant MY_PERMISSION_ACCESS_FINE_LOCATION
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSION_ACCESS_COARSE_LOCATION);
+            }
+            // The ACCESS_FINE_LOCATION is denied, then I request it and manage the result in
+            // onRequestPermissionsResult() using the constant MY_PERMISSION_ACCESS_FINE_LOCATION
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSION_ACCESS_FINE_LOCATION);
+            }
+
+        }
+    }
+
+    /**
+     * LocationListern监听器
+     * 参数：地理位置提供器、监听位置变化的时间间隔、位置变化的距离间隔、LocationListener监听器
+     */
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //如果位置发生变化,重新显示
+            String locationStr = "维度：" + location.getLatitude() + "\n"
+                    + "经度：" + location.getLongitude();
+            Message msg = new Message();
+            msg.what = 1;
+            mhandler.sendMessage(msg);
+
+        }
+    };
+
+
+    /**
+     * 获取天气信息
+     */
+    private void getWeatherData() {
         VolleyUtil volley = new VolleyUtil(this, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject jsonObject) {
-                Toast.makeText(MainActivity.this, jsonObject.toString(), Toast.LENGTH_SHORT).show();
+
+                Gson gson = new Gson();
+                ResponseTemplate response = gson.fromJson(jsonObject.toString(), ResponseTemplate.class);
+                updateDataToUI(response);
+
             }
         }, new Response.ErrorListener() {
             @Override
@@ -81,45 +323,25 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
-        volley.doGet();
+        if (!cityName.equals("")) {
+
+            volley.doGet(volley.getWeatherUrl(cityName));
+        }
+
     }
 
     /**
-     * 发送邮件
+     * 更新数据&&UI
      */
-    private void sendEmial() {
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:+297890152@qq.com"));
-        intent.putExtra(Intent.EXTRA_SUBJECT, "这是单方发送的邮件主题");
-        intent.putExtra(Intent.EXTRA_TEXT, "这是单方发送的邮件内容");
-        startActivity(intent);
-    }
-
-    /**
-     * 获取接口JSON
-     */
-    private void getData() {
-        String url = "http://wthrcdn.etouch.cn/weather_mini?city=北京";
-//        通过城市名字获得天气数据，json数据
-//        http://wthrcdn.etouch.cn/weather_mini?citykey=101010100
-//        通过城市id获得天气数据，json数据
-        RequestQueue mQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
-                        Log.i("==================", response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.i("TAG", error.getMessage(), error);
-                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-        mQueue.add(jsonObjectRequest);
-//        mQueue.stop();
+    private void updateDataToUI(ResponseTemplate response) {
+        cityName = response.getResult().getData().getRealtime().getCity_name();
+        temperature_now = response.getResult().getData().getRealtime().getWeather().getTemperature();
+        weather = response.getResult().getData().getRealtime().getWeather().getInfo();
+        tv_weather.setText(weather);
+        tv_city_name.setText(cityName);
+        tv_temperature.setText(temperature_now + "°");
+        initNearlyWeekWeather(response);
+        initHorizontalScrollView(response);
     }
 
 
@@ -127,21 +349,24 @@ public class MainActivity extends AppCompatActivity
      * 初始化下拉刷新
      */
     private void initPullToRefreshView() {
-        mPullToRefreshView = (PullToRefreshView) findViewById(R.id.pull_to_refresh);
-        mPullToRefreshView.setOnRefreshListener(new PullToRefreshView.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mPullToRefreshView.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(MainActivity.this, FullscreenActivity.class);
-                        startActivity(intent);
-                        mPullToRefreshView.setRefreshing(false);
-                    }
-                }, 1000);
-            }
-        });
+        mPullToRefreshView.setOnRefreshListener(refreshListener);
     }
+
+    /**
+     * 下拉刷新监听器
+     */
+    private PullToRefreshView.OnRefreshListener refreshListener = new PullToRefreshView.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+            mPullToRefreshView.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getWeatherData();
+                    mPullToRefreshView.setRefreshing(false);
+                }
+            }, 1000);
+        }
+    };
 
     @Override
     public void onBackPressed() {
@@ -217,4 +442,41 @@ public class MainActivity extends AppCompatActivity
         Toast.makeText(MainActivity.this, "点击", Toast.LENGTH_SHORT).show();
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_ACCESS_COARSE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                } else {
+                    // permission denied
+                }
+                break;
+            }
+            case MY_PERMISSION_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted
+                } else {
+                    // permission denied
+                }
+                break;
+            }
+
+        }
+    }
+
+    Handler mhandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 1:
+                    getWeatherData();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
